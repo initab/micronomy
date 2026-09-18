503313be928d453113ceea770152a740e0a6629e
aiquen <carl.lejdbrandt@b3.se>
2026-09-18 18:07:05 +0200

    Rebased on pipeline branch and acced in OIDC connection

---
+++ terraform/dev/github_aws_oidc.tf
@@ -0,0 +1,42 @@
+# GitHub Actions IAM Role Setup (OIDC) - Requires Customization
+# IMPORTANT: Update values below before deploying to production!
+#
+
+# 1. Create OIDC Provider
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["06d927fecd0a84aeba28aad1d808139470fe95c3"]
+}
+
+# 2. Create Role for GitHub Actions
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-ecr-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_org}/${var.github_repo}:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# 3. Attach ECR Permissions (or any other policy as needed)
+resource "aws_iam_role_policy_attachment" "ecr_permissions" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+}
--- terraform/dev/variables.tf
+++ terraform/dev/variables.tf
@@ -9,3 +9,15 @@ variable "app_service_desired_count" {
   type        = number
   default     = 1
 }
+
+variable "github_org" {
+  description = "GitHub organization name"
+  type        = string
+  default     = "initab"
+}
+
+variable "github_repo" {
+  description = "GitHub repository name"
+  type        = string
+  default     = "micronomy"
+}
